9a41ee00eab3d96777664d93a5c3e82ee9c1f54c
LeticiaGN <lentrylenlen@gmail.com>
2025-09-23 19:36:11 -0300

    Adicao da classe D

---
+++ D.java
@@ -0,0 +1,21 @@
+public class D {
+    private int D1;
+    private String D2;
+
+    public D() {}
+
+    public int getD1() { return D1; }
+    public void setD1(int d1) { this.D1 = d1; }
+
+    public String getD2() { return D2; }
+    public void setD2(String d2) { this.D2 = d2; }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
+
